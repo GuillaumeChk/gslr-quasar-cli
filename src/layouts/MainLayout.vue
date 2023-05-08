292aa21953c24cd58b799355817d4c3adc3d5474
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-05-08 13:57:31 +0200

    navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,32 +1,85 @@
 <template>
-  <div>baaaaaaaaaa</div>
   <q-layout view="lHh Lpr lFf">
-    <q-header style="top: 50px" elevated>
-      <q-toolbar>
+    <q-header elevated>
+      <q-toolbar class="wrapper">
         <q-btn
           flat
           dense
           round
+          class="lt-sm"
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-img
+          class="q-ma-md"
+          style="max-width: 200px; min-width: 50px"
+          src="../assets/quasar-logo-vertical.svg"
+          fit="contain"
+        ></q-img>
+        <q-toolbar-title>
+          <h3>GSLR Agencements</h3>
+          <p>Menuiserie — Agencements — Pose de cuisine — Sur mesure…</p>
+        </q-toolbar-title>
+        <div class="column justify-center">
+          <p>2000  Neuchâtel</p>
+          <p>TÉL. +41 XX XXX XX XX</p>
+          <p>Email contact@gslr.ch</p>
+        </div>
       </q-toolbar>
+
+      <q-tabs
+        v-model="tab"
+        no-caps
+        align="justify"
+        class="gt-xs text-white wrapper"
+      >
+        <q-route-tab
+          v-for="route in routesList"
+          :key="route.title"
+          :name="route.title"
+          exact
+          :to="route.to"
+        >
+          <span class="gt-sm biggerText">{{ route.title }}</span>
+          <span class="lt-md">{{ route.title }}</span>
+        </q-route-tab>
+        <!-- <q-route-tab name="unique" exact to="/prestations">
+          <span class="gt-sm biggerText">Unique</span>
+          <span class="lt-md">Unique</span>
+        </q-route-tab>
+        <q-route-tab name="loisirs" exact to="/loisirs">
+          <span class="gt-sm biggerText">Loisirs</span>
+          <span class="lt-md">Loisirs</span>
+        </q-route-tab>
+        <q-route-tab name="plaisirs" exact to="/plaisirs">
+          <span class="gt-sm biggerText">Plaisirs</span>
+          <span class="lt-md">Plaisirs</span>
+        </q-route-tab>
+        <q-route-tab name="autour" exact to="/autour">
+          <span class="gt-sm biggerText">Autour</span>
+          <span class="lt-md">Autour</span>
+        </q-route-tab> -->
+      </q-tabs>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer v-model="leftDrawerOpen" bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item
+          v-for="route in routesList"
+          clickable
+          tag="to"
+          :key="route.to"
+          :to="route.to"
+        >
+          <q-item-section v-if="route.icon" avatar>
+            <q-icon :name="route.icon" />
+          </q-item-section>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+          <q-item-section>
+            <q-item-label>{{ route.title }}</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -36,72 +89,44 @@
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
-import EssentialLink from "components/EssentialLink.vue";
+<script setup>
+import { ref } from "vue";
+// import EssentialLink from "components/EssentialLink.vue";
 
-const linksList = [
+let tab = ref("unique");
+
+const routesList = [
   {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
+    title: "Entreprise",
+    icon: "apartment",
+    to: "/",
   },
   {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
+    title: "Prestations",
+    icon: "design_services",
+    to: "/prestations",
   },
   {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
+    title: "Actualités",
+    icon: "newspaper",
+    to: "/actualites",
   },
   {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
+    title: "Contact",
+    icon: "contact_page",
+    to: "contact",
   },
 ];
 
-export default defineComponent({
-  name: "MainLayout",
+const leftDrawerOpen = ref(false);
 
-  components: {
-    EssentialLink,
-  },
-
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-    };
-  },
-});
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
 </script>
+
+<style>
+.biggerText {
+  font-size: 20px;
+}
+</style>
