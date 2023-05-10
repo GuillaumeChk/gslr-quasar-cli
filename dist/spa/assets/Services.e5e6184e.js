395e1f527e4f2c71ddf837ab37c4acc2641b5944
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-05-10 16:58:09 +0200

    try 2

--- dist/spa/assets/Services.e5e6184e.js
+++
@@ -1,1 +0,0 @@
-import{Q as t}from"./QPage.780c8a00.js";import{a0 as s,K as a,L as o,M as r,O as e}from"./index.4a5f4777.js";import"./render.efa1056d.js";const c={},n=e("h4",null,"Prestations",-1),_=e("p",null,"Exemples de prestations\u2026",-1);function i(l,p){return a(),o(t,null,{default:r(()=>[n,_]),_:1})}var u=s(c,[["render",i]]);export{u as default};
