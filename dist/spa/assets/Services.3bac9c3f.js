4a5f87f430a19ebfee48c7d534816804060132d5
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-05-10 15:22:41 +0200

    deploy 2

---
+++ dist/spa/assets/Services.3bac9c3f.js
@@ -0,0 +1,1 @@
+import{Q as t}from"./QPage.a1522ecf.js";import{a0 as s,K as a,L as o,M as r,O as e}from"./index.1b5d8b97.js";import"./render.8b11c449.js";const c={},n=e("h4",null,"Prestations",-1),_=e("p",null,"Exemples de prestations\u2026",-1);function i(l,p){return a(),o(t,null,{default:r(()=>[n,_]),_:1})}var u=s(c,[["render",i]]);export{u as default};
--- dist/spa/assets/Services.d97ace12.js
+++
@@ -1,1 +0,0 @@
-import{Q as t}from"./QPage.fe7efcc9.js";import{a0 as s,K as a,L as o,M as r,O as e}from"./index.5563492f.js";import"./render.f15d83b4.js";const c={},n=e("h4",null,"Prestations",-1),_=e("p",null,"Exemples de prestations\u2026",-1);function i(l,p){return a(),o(t,null,{default:r(()=>[n,_]),_:1})}var u=s(c,[["render",i]]);export{u as default};
