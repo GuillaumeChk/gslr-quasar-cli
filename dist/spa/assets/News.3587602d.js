a43cba586fc2dfd87e146047fcde7ca81fd0086c
GuillaumeChk <guillaume.chekhab39@gmail.com>
2023-05-12 14:28:17 +0200

    build

--- dist/spa/assets/News.3587602d.js
+++
@@ -1,1 +0,0 @@
-import{Q as t}from"./QPage.fc792678.js";import{a0 as s,K as r,L as o,M as a,O as e}from"./index.468a3d93.js";import"./render.d61e9de5.js";const c={},n=e("h4",null,"Actualit\xE9s",-1),_=e("p",null,"L'entreprise GSLR Agencements ouvre ses portes !",-1);function l(i,p){return r(),o(t,null,{default:a(()=>[n,_]),_:1})}var m=s(c,[["render",l]]);export{m as default};
